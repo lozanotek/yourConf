f9f5fc2ed732d947b8a760cea26cea0c4b609a8b
jglozano <javier@lozanotek.com>
2014-09-14 17:01:20 -0700

    Added video player layout and code to pull data from the VideoStreams.xml file to the main page.

--- src/Site/App_Code/Player.cs
+++ src/Site/App_Code/Player.cs
@@ -1,17 +1,43 @@
 ﻿using System;
 using System.Web;
 using System.Web.Caching;
+using System.Linq;
+using System.Xml.Linq;
 
 public static class Player {
     public static string GetVideoUrl() {
         var context = HttpContext.Current;
         var videoUrl = ConfContext.GetPlayerUrl();
 
-        if(string.IsNullOrEmpty(videoUrl)) {
-            videoUrl = ConfContext.PlayerUrl;
+        if(string.IsNullOrEmpty(videoUrl)){
+            var videoStream = GetVideoStream();
+            if(videoStream == null) return string.Empty;
+
+            videoUrl = string.Format("http://www.youtube.com/embed/{0}", videoStream.VideoId);
             ConfContext.SetPlayerUrl(videoUrl);
         }
 
         return videoUrl;
+    }
+
+    private static VideoStream GetVideoStream() {
+        var document = GetStreamDocument();
+        if(document == null) return null;
+        var rootElement = document.Root;
+
+        var query = from elem in rootElement.Elements()
+            select new VideoStream {
+                VideoId = elem.Attribute("videoId").Value
+         };
+
+        var list = query.ToList();
+         
+        return list[0];
+    }
+
+    private static XDocument GetStreamDocument() {
+       var path = ConfContext.PlayerUrl;
+       path = HttpContext.Current.Server.MapPath(path);
+       return XDocument.Load(path);      
     }
 }
---
+++ src/Site/App_Code/VideoStream.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+[Serializable]
+public class VideoStream {
+    public string VideoId {get; set;}
+}
